5110ac8ddbac38e25f570f5ad1d75828add940ac
CatalinVlad <cata_bv94@yahoo.com>
2015-09-28 10:29:44 +0300

    base app

--- js/modules/articole/views/layout.js
+++ js/modules/articole/views/layout.js
@@ -1,9 +1,24 @@
-define(function(require, exports, module) {
+define(function (require, exports, module) {
     var Backbone = require('backbone');
     var Marionette = require('marionette');
+
+    var ItemCollection = require('../models/ItemCollection');
+
+    var ItemCollectionView = require('./itemCollectionView');
 
     module.exports = Marionette.ItemView.extend({
         id: 'articole',
         template: '#articole-layout'
+
+        , initialize: function() {
+            var self = this;
+            Marionette.ItemView.prototype.initialize.apply(this, arguments);
+            this.collection = new ItemCollection();
+            var collectionView = new ItemCollectionView({collection:this.collection});
+            this.collection.fetch().then(function() {
+              // console.log(self.collection);
+                self.$el.find('.left-contain').html(collectionView.render().$el);
+            })
+        }
     });
 });
